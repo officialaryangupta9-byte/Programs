566f7631925607a0e1eed294a972311c1f57e0d4
officialaryangupta9-byte <officialaryangupta9@gmail.com>
2025-11-15 11:59:07 +0530

    Program to find the AASCI value of any character.

---
+++ AASCI.c
@@ -0,0 +1,12 @@
+
+//Program to find the AASCI value of any character.
+
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter the character : ");
+    scanf("%c", &ch);
+    printf("The AASCI value of %c is %d", ch, ch);
+    return 0;
+}
