1b397dc96f087af9639ba6f4c8ff086444048a22
officialaryangupta9-byte <officialaryangupta9@gmail.com>
2025-11-15 12:08:55 +0530

    Program to print the sum of two number.

---
+++ Sum2no.c
@@ -0,0 +1,13 @@
+
+//Program to print the sum of two number.
+
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    printf("Enter first number : ");
+    scanf("%d", &n1);
+    printf("Enter second number : ");
+    scanf("%d", &n2);
+    printf("The sum of two numbers is : %d", n1 + n2);
+    return 0;
+}
