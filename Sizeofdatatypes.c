58b1fe5bd843d32f4793327e2a4759c4cab0bb50
officialaryangupta9-byte <officialaryangupta9@gmail.com>
2025-11-15 12:08:07 +0530

    Program to print the size of all the datatypes.

---
+++ Sizeofdatatypes.c
@@ -0,0 +1,13 @@
+
+//Program to print the size of all the datatypes.
+
+
+#include <stdio.h>
+int main() {
+    printf("The size of char datatype is %d byte", sizeof(char));
+    printf("\nThe size of int datatype is %d bytes", sizeof(int));
+    printf("\nThe size of float datatype is %d bytes", sizeof(float));
+    printf("\nThe size of double datatype is %d bytes", sizeof(double));
+    printf("\nThe size of long double datatype is %d bytes", sizeof(long double));
+    return 0;
+}
